5aeecc16816c2d9a9179d7ae6110633aa36de720
remche <remche@remche.org>
2020-06-08 17:56:18 +0200

    remove k8s sed from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python -m bash_kernel.install
-sed -i 's/Bash/K8S/g' ~/.local/share/jupyter/kernels/bash/kernel.json
 if [ -x /entrypoint_override.sh ]; then
   /entrypoint_override.sh
 fi
